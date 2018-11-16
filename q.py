931ab8a2d3bb1b2682118ddba6813edfaaa0c390
Aui Mendoza <auimendoza@gmail.com>
2018-11-15 23:28:40 -0500

    q ok

---
+++ q.py
@@ -0,0 +1,127 @@
+#!/Users/auimendoza/anaconda/envs/py36/bin/python
+
+import gym
+import numpy as np
+from mdptoolbox.mdp import ValueIteration
+from mdplib import PolicyIteration, QLearning
+import common
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+def Usage():
+  print("Usage:")
+  print("./%s <envid>" % (sys.argv[0]))
+  sys.exit(1)
+
+if len(sys.argv) < 2:
+  Usage()
+
+envid = int(sys.argv[1])
+
+maxiter = 2000000
+interval = 250000
+gammas = [0.1, 0.3, 0.5, 0.7, 0.9]
+episodes = 1000
+alphas = [0.1, 0.3, 0.5, 0.7, 0.9]
+
+env, envname, P, R, actions, actions2 = common.getEnv(envid)
+
+print("* %s *" % (envname))
+print("Q Learning")
+
+aiters = []
+aghls = []
+ats = []
+amd = []
+bestgoal = 0
+bestpolicy = None
+bestpolicyparams = {}
+for alpha in alphas:
+  iters = []
+  ghls = []
+  ts = []
+  md = []
+  for gamma in gammas:
+    print("gamma: %.1f, alpha: %s" % (gamma, str(alpha)))
+    func = QLearning(P, R, gamma, maxiter, interval, alpha)
+    func.run()
+    ighl = []
+    its = []
+    for i, policy in enumerate(func.policies):
+      timesteps, gtimesteps, ghl = common.runPolicy(env, episodes, policy)
+      if ghl[0] > bestgoal:
+        bestgoal = ghl[0]
+        bestpolicy = policy
+        bestpolicyparams['gamma'] = gamma
+        bestpolicyparams['alpha'] = alpha
+        bestpolicyparams['iterations'] = func.iterations[i]
+        bestpolicyparams['elapsedtime'] = func.elapsedtimes[i]
+        bestpolicyparams['meangtimesteps'] = np.mean(gtimesteps)
+      ighl.append(ghl)
+      its.append(np.mean(timesteps))
+    iters.append(func.iterations)
+    ghls.append(ighl)
+    ts.append(its)
+    md.append(func.mean_discrepancy)
+  aiters.append(iters)
+  aghls.append(ghls)
+  ats.append(ts)
+  amd.append(md)
+
+# plot best policy 
+bestparamstext = "gamma=%.1f\nalpha=%.1f\niterations=%d\nelapsed time=%.3f\nmean goal timesteps=%.3f" % (bestpolicyparams['gamma'],
+    bestpolicyparams['alpha'],
+    bestpolicyparams['iterations'],
+    bestpolicyparams['elapsedtime'],
+    bestpolicyparams['meangtimesteps'])
+common.printNicePolicy(env, bestpolicy, actions2, 20, 4.75, 1, "QLearning: Best Policy\n%s" % (envname), bestparamstext, "%d-q-bestpolicy.png" % (envid))
+
+# plot iterations, params vs goal/discrepancy
+iterations = aiters[0][0]
+ghls = []
+aghl = np.array(aghls)
+for i in range(aghl.shape[0]):
+    for j in range(aghl.shape[1]):
+        for k in range(aghl.shape[2]):
+            ghls.append([alphas[i], gammas[j], iterations[k], aghl[i,j,k,0], aghl[i,j,k,1], aghl[i,j,k,2], np.array(amd)[i,j,k], np.array(ats)[i,j,k]])
+ghlpd = pd.DataFrame(ghls, columns=['alpha', 'gamma', 'i', 'goal', 'hole', 'lost', 'md', 'ts'])
+ghlpd['param'] = ghlpd.apply(lambda row: 'alpha=' + str(row['alpha']) + ', gamma=' + str(row['gamma']), axis=1)
+
+g = sns.FacetGrid(ghlpd, col="alpha", hue="gamma", col_wrap=5, legend_out=False)
+g = g.map(plt.plot, "i", "goal", marker=".")
+g.add_legend()
+g.set_xlabels('iterations')
+g.set_ylabels('goals')
+g.fig.subplots_adjust(top=0.7)
+g.fig.suptitle('Goals After %d Episodes\n%s' % (episodes, envname))
+g.set_xticklabels(rotation=90)
+plt.gcf()
+plt.savefig("%d-q-goal-it.png" % (envid), bbox_inches="tight")
+plt.close()
+
+g = sns.FacetGrid(ghlpd, col="alpha", hue="gamma", col_wrap=5, legend_out=False)
+g = g.map(plt.plot, "i", "md", marker=".")
+g.add_legend()
+g.set_xlabels('iterations')
+g.set_ylabels('Q mean discrepancy')
+g.fig.subplots_adjust(top=0.7)
+g.fig.suptitle('Q Mean Discrepancy\n%s' % (envname))
+g.set_xticklabels(rotation=90)
+plt.gcf()
+plt.savefig("%d-q-md-it.png" % (envid), bbox_inches="tight")
+plt.close()
+
+g = sns.FacetGrid(ghlpd, col="alpha", hue="gamma", col_wrap=5, legend_out=False)
+g = g.map(plt.plot, "i", "ts", marker=".")
+g.add_legend()
+g.set_xlabels('iterations')
+g.set_ylabels('timesteps')
+g.fig.subplots_adjust(top=0.7)
+g.fig.suptitle('Timesteps to Goal or Hole\n%s' % (envname))
+g.set_xticklabels(rotation=90)
+g.set(ylim=(0,200))
+plt.gcf()
+plt.savefig("%d-q-ts-it.png" % (envid), bbox_inches="tight")
+plt.close()
